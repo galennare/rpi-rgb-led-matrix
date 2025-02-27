a5e7d6de1ffb58caceebf18eed21209bb1747b0e
Galen Nare <30024882+galennare@users.noreply.github.com>
2025-02-27 12:48:18 -0500

    platformio due project

---
+++ arduino_due_rgb_display/src/led-matrix.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+#include "led-matrix.h"
+
+using std::string;
+
+namespace rgb_matrix {
+    bool rgb_matrix::RGBMatrix::Options::Validate(string *err) const {
+        
+    }
+}  // end namespace rgb_matrix
